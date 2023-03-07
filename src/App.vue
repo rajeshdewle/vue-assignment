6511e1fd11c38209cce9e3ef6013e4f21b6e680c
Rajesh Dewle <rdewle@entrib.com>
2023-03-07 12:12:27 +0530

    Add boostrap and sass-loader

--- src/App.vue
+++ src/App.vue
@@ -14,13 +14,13 @@ export default {
 }
 </script>
 
-<style>
+<style lang="scss">
+@import '../node_modules/bootstrap/scss/bootstrap.scss';
+
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
 }
 </style>
